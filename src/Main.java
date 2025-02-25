e2fe11935dbdcdb7e6a56b5270cb5d708a19ed75
robertobzrr <robertobezerrafh@outlook.com>
2025-02-25 09:04:25 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Nums n = new Nums();
+        int i = 0;
+
+        do{
+
+        System.out.println("Write one number: ");
+        n.num1 = sc.nextInt();
+
+        System.out.println("Write another number: ");
+        n.num2 = sc.nextInt();
+
+        System.out.println("Choose what you want to do with the 2 numbers:\n[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Divison");
+
+
+        int calc = sc.nextInt();
+        switch (calc) {
+            case 1:
+                System.out.println("The addition result: " + n.add(n.num1, n.num2));
+                break;
+
+            case 2:
+                System.out.println("The subtration result: " + n.sub(n.num1, n.num2));
+                break;
+
+            case 3:
+                System.out.println("The multiplication result: " + n.multi(n.num1, n.num2));
+                break;
+
+            case 4:
+                System.out.println("The division result: " + n.div(n.num1, n.num2));
+                break;
+
+        }
+
+        System.out.println("[0] if you want to continue");
+        i = sc.nextInt();
+        }while (i == 0);
+
+        System.out.println("The End!");
+
+    }
+}
+
+
+
+
+      /*      case 2:
+                n.sub();
+                System.out.println("The subtration result: "+ n.sub);
+                break;
+
+            case 3:
+                n.mult();
+                System.out.println("The multiplication result: "+ n.mult);
+                break;
+
+            case 4:
+                n.div();
+                System.out.println("The division result: "+ n.div);
+                break;
+*/
---
+++ src/Nums.java
@@ -0,0 +1,52 @@
+public class Nums {
+
+
+    double num1;
+    double num2;
+    double addR;
+    double subR;
+    double multiR;
+    double divR;
+
+
+public double add(double num1, double num2){
+    return addR = num1 + num2;
+}
+
+public double sub(double num1, double num2){
+    return subR = num1 - num2;
+}
+
+public double multi(double num1, double num2){
+    return multiR = num1 * num2;
+}
+
+public double div(double num1, double num2){
+    return divR = num1 / num2;
+}
+
+
+/*
+    double add;
+    public void add(){
+        add = num1 + num2;
+    }
+
+
+    double sub;
+    public void sub(){
+        sub = num1 - num2;
+    }
+
+
+    double mult;
+    public void mult(){
+        mult = num1 * num2;
+    }
+
+
+    double div;
+    public void div(){
+        div = num1 / num2;
+    }*/
+}
