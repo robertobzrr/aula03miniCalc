0724776c8f58be6d33ae72bd5910e0b711ce7e6f
robertobzrr <robertobezerrafh@outlook.com>
2025-02-25 21:30:21 -0300

    indentation

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Nums n = new Nums();
-        int i = 0;
+            Scanner sc = new Scanner(System.in);
+            Nums n = new Nums();
+            int i = 0;
 
         do{
 
-        System.out.println("Write one number: ");
-        n.n1 = sc.nextInt();
+            System.out.println("Write one number: ");
+            n.n1 = sc.nextInt();
 
-        System.out.println("Write another number: ");
-        n.n2 = sc.nextInt();
+            System.out.println("Write another number: ");
+            n.n2 = sc.nextInt();
 
-        System.out.println("Choose what you want to do with the 2 numbers:\n[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Divison");
+            System.out.println("Choose what you want to do with the 2 numbers:\n[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Divison");
 
 
-        int calc = sc.nextInt();
-        switch (calc) {
-            case 1:
-                System.out.println("The addition result: " + n.add(n.n1, n.n2));
-                break;
+                int calc = sc.nextInt();
+                switch (calc) {
+                    case 1:
+                        System.out.println("The addition result: " + n.add(n.n1, n.n2));
+                        break;
 
-            case 2:
-                System.out.println("The subtration result: " + n.sub(n.n1, n.n2));
-                break;
+                    case 2:
+                        System.out.println("The subtration result: " + n.sub(n.n1, n.n2));
+                        break;
 
-            case 3:
-                System.out.println("The multiplication result: " + n.multi(n.n1, n.n2));
-                break;
+                    case 3:
+                        System.out.println("The multiplication result: " + n.multi(n.n1, n.n2));
+                        break;
 
-            case 4:
-                System.out.println("The division result: " + n.div(n.n1, n.n2));
-                break;
+                    case 4:
+                        System.out.println("The division result: " + n.div(n.n1, n.n2));
+                        break;
 
-        }
+                }
 
         System.out.println("[0] if you want to continue");
         i = sc.nextInt();
